6cfc6ab7d443b0c6adb09f31889a966fe2025be5
ozcanakyuz <xe_bilmece@hotmail.com>
2024-10-24 15:44:03 +0300

    08_Methods
    08_Methods

---
+++ 08_Methods/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //void CustomerList()
+            //{
+            //    Console.WriteLine("Ali YILDIZ");
+            //    Console.WriteLine("Ayşe YILDIZ");
+            //    Console.WriteLine("Hakan OZTURK");
+            //    Console.WriteLine("Merve CINAR");
+            //}
+            //CustomerList();
+
+
+            string StudentCard()
+            {
+                string name = "ALI";
+                string surname = "KAYA";
+
+                return name + " " + surname;
+            }
+
+            Console.WriteLine(StudentCard());
+
+            Console.Read();
+        }
+    }
+}
